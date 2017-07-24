c678d8db702b371d4c3ecd6ecd353c5379849c5f
ChenShuyao <cshuyaoxx@gmail.com>
2017-07-24 23:01:21 +0100

    modify the variables

--- gpu_compile.m
+++ gpu_compile.m
@@ -3,10 +3,10 @@ addpath examples;
 run vlfeat/toolbox/vl_setup ;
 run matlab/vl_setupnn ;
 
-vl_compilenn('enableGpu', true, ...
-'cudaRoot', '/usr/local/cuda/', ...  %change it 
-'cudaMethod', 'nvcc',...
-'enableCudnn',true,... 
+vl_compilenn('enableGpu', true, 
+'cudaRoot', '/usr/local/cuda/', 
+'cudaMethod', 'nvcc',
+'enableCudnn',true,
 'cudnnroot','/opt/cuda/cudnn-5.0/');
-%}
+
 warning('off');
